c65f9f4aa7d84d2ccff6a8a97e5cd93710f343d9
saugat1070 <saugatgiri1070@gmail.com>
2025-03-12 14:29:56 +0545

    completing condition in react

--- React_Learning/src/compenets/ProductTab.jsx
+++ React_Learning/src/compenets/ProductTab.jsx
@@ -1,10 +1,15 @@
 import { Product } from "./product"
 // --> props are the information that you pass to a jsx tag
 function ProductTab(){ 
+    let option  = ["hi-teach","durable","fast"];
+    let option2 = {a:"hi-tech",b:"durable",c:"fast"};
+    let list_ = [<li>add component</li>,<li>delete component</li>];
     return(
         <div className="ProductTab">
-            <Product title="Iphone 14" Price={10000}/>
-            <Product title="Samsung S24" Price={12000}/>
+            <Product title="Iphone 14" Price={10000} features={option} feature2={option2} list_li={list_}/>
+            <Product title="Samsung S24" Price={12000} features={option} feature2={option2} list_li={list_}/>
+            <Product title="Vivobook" Price={120000} features={option} feature2={option2} list_li={list_}/>
+
         </div>
     );
 }
