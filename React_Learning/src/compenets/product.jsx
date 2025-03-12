f2478d1b8e9e081920de632ef7107e663dd8a84c
saugat1070 <saugatgiri1070@gmail.com>
2025-03-12 14:21:47 +0545

    completing mapping method in component

--- React_Learning/src/compenets/product.jsx
+++ React_Learning/src/compenets/product.jsx
@@ -8,12 +8,37 @@ import "./product.css"
 //         </div>
 //     );
 // }
-function Product({title,Price=0}){  //{title,price} <-- React Props
+
+// function Product({title,Price=0,features,feature2,list_li}) {  //{title,price} <-- React Props
+//     console.log(list_li);
+//     return(
+//         <div className  ="Product">
+//             <h3>Product:{title}</h3>
+//             <h5>Price:{Price}</h5>
+//             <p>{list_li}</p>
+//             <p>{feature2.a}</p>
+//         </div>
+//     )
+// }
+// function Product({title,Price,features}){
+//     const list = features.map((feature)=> <li>{feature}</li>); // <-- mapping method
+//     return(
+//         <div className="Product">
+//         <h3>title:{title}</h3>
+//         <h4>price:{Price}</h4>
+//         <p>{list}</p>
+//         </div>
+//     )
+// }
+function Product({title,Price}){
+    
     return(
         <div className="Product">
-            <h3>Product:{title}</h3>
-            <h5>Price:{Price}</h5>
+        <h3>title:{title}</h3>
+        <h4>price:{Price}</h4>
+        
         </div>
     )
 }
+
 export { Product };
