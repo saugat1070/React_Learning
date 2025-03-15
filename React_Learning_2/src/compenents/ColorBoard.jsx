6883b19f72ea443967ff8ee6d2b25d46b5f4ef28
saugat1070 <saugatgiri1070@gmail.com>
2025-03-15 23:42:28 +0545

    adding colorboard

---
+++ React_Learning_2/src/compenents/ColorBoard.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+// export default function ColorBoard({color_choice}){
+//     console.log(color_choice); 
+//     let [count,setCount] = useState(0);
+//     let move = ()=>{
+//             setCount(count+1);
+//         }
+//     return (
+//         <div className="color_board">
+//             <p>{color_choice} moves: {count}</p>
+//             <div onClick={move}>+1</div>
+//         </div>
+//     )
+// }
+
+// import { useState } from "react";
