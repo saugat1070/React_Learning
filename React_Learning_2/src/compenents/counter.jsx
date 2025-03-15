5de67ca51d293da96ba8eb08702b454f13ac4bca
saugat1070 <saugatgiri1070@gmail.com>
2025-03-15 23:42:45 +0545

    adding counter

--- React_Learning_2/src/compenents/counter.jsx
+++ React_Learning_2/src/compenents/counter.jsx
@@ -4,8 +4,16 @@ function Counter(){
     let [Count,setCount] = useState(0);
     console.log("Component is rendered!");
     let inCount = ()=>{
-       setCount(Count+1); 
-    }
+       setCount((currValue)=>{ //usestate with arrow function used to callback useState
+        return currValue +1;// in this program ,count incrase by 3
+       });  
+       setCount((currValue)=>{
+        return currValue +1;
+       });
+       setCount((currValue)=>{
+        return currValue +1;
+       });  
+    }  
     return(
         <div className="Counter">
             <h3>count:{Count}</h3>
