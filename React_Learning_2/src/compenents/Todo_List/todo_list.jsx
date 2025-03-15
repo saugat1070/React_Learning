beeddb70c391c60c09f3c8adcd2a3b86b59f6e7e
saugat1070 <saugatgiri1070@gmail.com>
2025-03-16 01:00:33 +0545

    we add delete method and update method for all objects using object spreading method and callbacks methos

--- React_Learning_2/src/compenents/Todo_List/todo_list.jsx
+++ React_Learning_2/src/compenents/Todo_List/todo_list.jsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
  export default function Todo_List(){
 
     // let [todos,setTodos] = useState(["sample Task","Hi hello"]); <-- array method
-    let [todos,setTodos] = useState([{task:"sample of task",id:uuidv4()},]);
+    let [todos,setTodos] = useState([{task:"sample of task",id:uuidv4()},]); // <-- array of object method
     let [newtodo,setNewtodo] = useState("");
 
     let addNewTask = () =>{
@@ -17,6 +17,38 @@ import { v4 as uuidv4 } from 'uuid';
         setNewtodo(event.target.value);
 
     }
+    let delete_task = (id)=>{
+        // let copy = todos.filter((todo)=> todo.id != todo);
+        // console.log(copy);
+        setTodos((preTodo) =>
+            todos.filter((preTodo) => preTodo.id != id)
+        )
+    }
+
+    let upperCaseAll = () =>{
+        /* let newArr = todos.map((todo) => {
+            // console.log(todo.task.toUpperCase());
+            return { 
+                ...todo,
+                task:todo.task.toUpperCase(),
+                
+
+            }
+        });
+        //  console.log(newArr);
+        setTodos(newArr); */
+        setTodos((preValue)=>(
+            //for call we use this arrow function
+            preValue.map((todo) =>{
+                return {
+                    ...todo,
+                    task:todo.task.toUpperCase(),
+                }
+            })
+        ))
+
+    }
+
     return (
         <div className="Todo_List">
             <h1>Todo List</h1>
@@ -29,9 +61,14 @@ import { v4 as uuidv4 } from 'uuid';
             <ul>
                 {
                     todos.map((todo) => (
-                        <li key={todo.id}>{todo.task}</li>
+                        <li key={todo.id}>
+                            <span>
+                            {todo.task}
+                            </span> <button onClick={()=> delete_task(todo.id)}>delete</button>
+                            </li>
                     ))
                 }
+                <button onClick={upperCaseAll}>uppercase</button>
             </ul>
         </div>
     )
