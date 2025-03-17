09ac8224f1b7abc876a5173b46dc8bfdaabed1d5
saugat1070 <saugatgiri1070@gmail.com>
2025-03-17 15:59:07 +0545

    small change in App module

--- Lottery_Game/src/App.jsx
+++ Lottery_Game/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Lottery from './Components/lottery'
+// import Lottery from './Components/lottery'
+import Lottery_Interface from './Components/lottery/lottery'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Lottery/>
+      {/* <Lottery/> */}
+      <Lottery_Interface size_of_ticket={3} />
+      <Lottery_Interface size_of_ticket = {5} />
     </>
   )
 }
