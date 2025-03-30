5cb6a708cbd7a3bb7cb7b0857fac676eedb9ae0b
saugat1070 <saugatgiri1070@gmail.com>
2025-03-31 02:26:38 +0545

    setup ui for redux learning in react

---
+++ Redux_Learning/Redux_learn/Namaskar_React/src/pages/blog/AddBlog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Layout from '../../components/Layout'
+
+function AddBlog() {
+  return (
+    <Layout>
+            
+    </Layout>
+    
+  )
+}
+
+export default AddBlog
