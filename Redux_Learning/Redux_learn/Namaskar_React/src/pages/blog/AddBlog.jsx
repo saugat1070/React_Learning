26c2cdcdfc2168528b8ace2fc30cb1026b922e1c
saugat1070 <saugatgiri1070@gmail.com>
2025-04-03 03:19:39 +0545

    adding form in this adding blog but remaining to correct

--- Redux_Learning/Redux_learn/Namaskar_React/src/pages/blog/AddBlog.jsx
+++ Redux_Learning/Redux_learn/Namaskar_React/src/pages/blog/AddBlog.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import Layout from '../../components/Layout'
-
+import Form from '../../components/Form'
 function AddBlog() {
   return (
     <Layout>
-            
+            <Form title="Create Blog">
+              
+            </Form>
     </Layout>
     
   )
