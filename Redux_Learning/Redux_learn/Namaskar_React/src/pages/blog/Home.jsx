d46884c46654c780c2540fd9a9daa6d15ea6a409
saugat1070 <saugatgiri1070@gmail.com>
2025-04-10 10:23:47 +0545

    completing home

--- Redux_Learning/Redux_learn/Namaskar_React/src/pages/blog/Home.jsx
+++ Redux_Learning/Redux_learn/Namaskar_React/src/pages/blog/Home.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Layout from '../../components/Layout'
-import Card from '../../components/card'
+import React, { useEffect } from 'react';
+import Layout from '../../components/Layout';
+import Card from '../../components/card';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBlog } from '../../../store/blogSlice';
+import { Link } from 'react-router-dom';
 
 function Home() {
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.blog);
+
+  useEffect(() => {
+    dispatch(fetchBlog());
+  }, [dispatch]);
+
+  console.log(data)
   return (
-    <Layout >
-        <div className=' ml-5 flex flex-row flex-wrap'>
-        <Card/>
-        <Card />
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        </div>
-    </Layout> 
-    )
+    <Layout>
+      <div className="ml-5 flex justify-center flex-wrap space-x-5">
+        {data && data.length > 0 ? (
+          data.map((blog) => (
+            <Card key={blog.id} blogs={blog} />
+          ))
+        ) : (
+          <p>No blogs available</p>
+        )}
+      </div>
+    </Layout>
+  );
 }
 
-export default Home
+export default Home;
