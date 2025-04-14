191303f9cf3c4854807def0c71ea37a42323120e
saugat1070 <saugatgiri1070@gmail.com>
2025-04-14 08:27:52 +0545

    correction on card

--- Redux_Learning/Redux_learn/Namaskar_React/src/components/card.jsx
+++ Redux_Learning/Redux_learn/Namaskar_React/src/components/card.jsx
@@ -10,16 +10,16 @@ return (
             <img className="rounded-t-lg" src={blogs.imageUrl} alt="hi" />
         </Link>
         <div className="p-5">
-            <a href="#">
+            <Link to='#'>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                      {blogs.title}
 
                 </h5>
-            </a>
+            </Link>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {blogs.description}            </p>
-            <a
-                href="#"
+            <Link
+                to="#"
                 className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
                 Read more
@@ -38,7 +38,7 @@ return (
                         d="M1 5h12m0 0L9 1m4 4L9 9"
                     />
                 </svg>
-            </a>
+            </Link>
         </div>
     </div>
     </Link>
